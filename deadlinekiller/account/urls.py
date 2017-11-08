be317eff45254f3ef23bc0f241e88758c3b53436
lucaromanowski <lucaromanowski@wp.pl>
2017-11-08 13:36:04 +0100

    password change - not working yet

--- deadlinekiller/account/urls.py
+++ deadlinekiller/account/urls.py
@@ -3,6 +3,8 @@ from django.contrib.auth import views as auth_views
 
 from . import views
 
+
+
 urlpatterns = [
     #url(r'^login/$', views.user_login, name='login'),
     
@@ -12,6 +14,10 @@ urlpatterns = [
     url(r'^logout/$',  auth_views.LogoutView.as_view(), name='logout'),
     # Logout then login
     url(r'^logout-then-login/$', auth_views.logout_then_login, name='logout_than_login'),
+
+    # Password change
+    url(r'^password-change/$', auth_views.PasswordChangeView.as_view(), name='password_change'),
+    url(r'^password-change/done/$', auth_views.PasswordChangeDoneView.as_view(), name='password_change_done'),
 
 
 
--- deadlinekiller/deadlinekiller/settings.py
+++ deadlinekiller/deadlinekiller/settings.py
@@ -33,13 +33,14 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    'account',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'account',
+    #'account',
     'deadlines',
 ]
 
