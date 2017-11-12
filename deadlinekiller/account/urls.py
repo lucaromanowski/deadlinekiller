93c02028fec3c51971bc79c32ad0d1d45f4e8026
lucaromanowski <lucaromanowski@wp.pl>
2017-11-12 19:52:47 +0100

    user registration done

--- deadlinekiller/account/urls.py
+++ deadlinekiller/account/urls.py
@@ -19,11 +19,14 @@ urlpatterns = [
     url(r'^password-change/$', auth_views.PasswordChangeView.as_view(), name='password_change'),
     url(r'^password-change/done/$', auth_views.PasswordChangeDoneView.as_view(), name='password_change_done'),
     
-    # Reseting password 	
+    # Resetting password 	
     url(r'^password-reset/$', auth_views.PasswordResetView.as_view(), name='password_reset'),
     url(r'^password-reset/done$', auth_views.PasswordResetDoneView.as_view(), name='password_reset_done'),
     url(r'^password-reset/confirm/(?P<uidb64>[-\w]+)/(?P<token>[-\w]+)/$', auth_views.PasswordResetConfirmView.as_view(), name='password_reset_confirm'),
     url(r'^password-reset/complete/$', auth_views.PasswordResetCompleteView.as_view(), name='password_reset_complete'),
+
+    # Register user
+    url(r'^register/$', views.register, name='register'),
 
 
 
--- deadlinekiller/account/views.py
+++ deadlinekiller/account/views.py
@@ -1,7 +1,9 @@
 from django.http import HttpResponse
 from django.shortcuts import render
 from django.contrib.auth import authenticate, login 
-from .forms import LoginForm
+
+from .forms import LoginForm, UserRegistrationForm
+
 
 def user_login(request):
 	if request.method == 'POST':
@@ -22,6 +24,19 @@ def user_login(request):
 		form = LoginForm()
 	return render(request, 'account/login.html', {'form' : form})
 
+
+def register(request):
+	if request.method == 'POST':
+		user_form = UserRegistrationForm(request.POST) 
+		if user_form.is_valid():
+			new_user = user_form.save(commit=False)
+			# Set given password
+			new_user.set_password(user_form.cleaned_data['password'])
+			new_user.save()
+			return render(request, 'account/register_done.html', { 'new_user' : new_user })
+	else:
+		user_form = UserRegistrationForm()
+	return render(request, 'account/register.html', { 'user_form': user_form })
 
 
 
