dd013d9b8c1c9f8268785a39b265245e92617f4a
lucaromanowski <lucaromanowski@wp.pl>
2017-11-16 21:33:00 +0100

    deadine model

--- deadlinekiller/deadlines/models.py
+++ deadlinekiller/deadlines/models.py
@@ -1,3 +1,42 @@
+from django.contrib.auth.models import User
 from django.db import models
 
-# Create your models here.
+
+
+
+
+
+class Project(models.Model):
+	pass
+
+
+
+class Deadline(models.Model):
+	'''
+	This model represents deadline.
+	The focal point of the app.
+	'''
+	PRIORITIES = (
+			('1', 'highest'),
+			('2', 'high'),
+			('3', 'medium'),
+			('4', 'low'),
+			('5', 'lowest'),
+
+		)
+
+	name = models.CharField(max_length=80)
+	slug = models.SlugField(max_length=80)
+	description = models.CharField(max_length=200)
+	project = models.ForeignKey(Project, related_name='deadlines', blank=True, null=True)
+	author = models.ForeignKey(User, related_name='deadlines')
+	deadline_date = models.DateTimeField()
+	created = models.DateTimeField(auto_now_add=True)
+	updated = models.DateTimeField(auto_now=True)
+	priority = models.CharField(max_length=80, choices=PRIORITIES, default='3')	
+
+
+	def __str__(self):
+		return '{} created by {}'.format(self.title, self.author)
+
+
