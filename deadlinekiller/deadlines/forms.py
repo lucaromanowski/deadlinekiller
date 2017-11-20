7944972a94bc69b966d9aeec20bfcf2781c1d6b6
lucaromanowski <lucaromanowski@wp.pl>
2017-11-20 22:58:02 +0100

    remove author form deadline form

--- deadlinekiller/deadlines/forms.py
+++ deadlinekiller/deadlines/forms.py
@@ -4,13 +4,6 @@ from .models import Deadline
 
 
 class DeadlineForm(forms.ModelForm):
-
-#	def __init__(self, *args, **kwargs):
-#		self.user = kwargs.pop('user', None)
-#		super(DeadlineForm, self).__init__(*args, **kwargs)
-#		print('Form init methd')
-
-
 	class Meta:
 		model = Deadline
-		fields = ('name', 'description', 'project', 'deadline_date', 'priority', 'author')
+		fields = ('name', 'description', 'project', 'deadline_date', 'priority')
