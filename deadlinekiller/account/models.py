442b1a023c7358c5c0a9b1a62fdd9ce78b27d26d
lucaromanowski <lucaromanowski@wp.pl>
2017-12-06 01:21:41 +0100

    connection model made

--- deadlinekiller/account/models.py
+++ deadlinekiller/account/models.py
@@ -1,6 +1,9 @@
 from django.conf import settings
 from django.db import models
 
+from friends.models import Connection
+
+
 #from teams.models import Team
 
 
@@ -10,6 +13,16 @@ class Profile(models.Model):
 	photo = models.ImageField(upload_to='users/%Y/%m/%d', blank=True)
 	# from Teams app
 	#team = models.ManyToManyField(Team, blank=True)
+
+
+	def get_connections(self):
+		connections = Connection.objects.filter(creator=self.user)
+		return connections
+
+
+	def get_followers(self):
+		followers = Connection.objects.filter(following=self.user)
+		return followers
 
 
 	def __str__(self):
--- deadlinekiller/friends/models.py
+++ deadlinekiller/friends/models.py
@@ -1,3 +1,12 @@
+from django.contrib.auth.models import User
 from django.db import models
 
-# Create your models here.
+
+class Connection(models.Model):
+	 created = models.DateTimeField(auto_now_add=True, editable=False)
+	 creator = models.ForeignKey(User, related_name='friendship_creator_set')
+	 following = models.ForeignKey(User, related_name='friend_set')
+	 accepted = models.BooleanField(default=False)
+
+	 def __str__(self):
+	 	return 'Connection between {} and {}'.format(str(creator.username), str(following.username))
