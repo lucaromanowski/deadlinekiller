fd1f8de30527d54b7731b2a43311f523d060d3c8
lucaromanowski <lucaromanowski@wp.pl>
2017-12-07 02:12:30 +0100

    Q lookups

--- deadlinekiller/account/models.py
+++ deadlinekiller/account/models.py
@@ -4,13 +4,14 @@ from django.db import models
 from friends.models import Connection
 
 
-#from teams.models import Team
-
 
 class Profile(models.Model):
 	user = models.OneToOneField(settings.AUTH_USER_MODEL)
 	date_of_birth = models.DateField(blank=True, null=True)
 	photo = models.ImageField(upload_to='users/%Y/%m/%d', blank=True)
+	# In future (# add profile bio lookup in future in friends app)
+	# bio = models.TextField(blank=True, null=True)
+
 	# from Teams app
 	#team = models.ManyToManyField(Team, blank=True)
 
--- deadlinekiller/friends/views.py
+++ deadlinekiller/friends/views.py
@@ -1,4 +1,5 @@
 from django.contrib.auth.models import User
+from django.db.models import Q
 from django.shortcuts import render
 from django.views.generic import ListView
 
@@ -14,6 +15,15 @@ class Connections(ListView):
 		context = super(Connections, self).get_context_data()
 		query = self.request.GET.get("q")
 		if query:
-			users = User.objects.filter(username__icontains=query).exclude(pk=self.request.user.pk)
+			users = User.objects.filter(
+										Q(username__icontains=query) | 
+										Q(email__icontains=query) |
+										Q(first_name__icontains=query) |
+										Q(last_name__icontains=query) | 
+										Q(profile__date_of_birth__icontains=query) 
+										).distinct().exclude(pk=self.request.user.pk) # add profile bio lookup in future
+			
+
+
 			context['users'] = users		
 		return context
