a86fa48e95e9a45b929ec71a80997e6f78c60cc0
lucaromanowski <lucaromanowski@wp.pl>
2017-12-07 01:44:17 +0100

    searching friends - simple

--- deadlinekiller/friends/views.py
+++ deadlinekiller/friends/views.py
@@ -1,10 +1,19 @@
+from django.contrib.auth.models import User
 from django.shortcuts import render
 from django.views.generic import ListView
 
-from .models import Connection
+from .models import Connection 
 
 
 class Connections(ListView):
 	model = Connection
 	template_name = 'friends/connections_list.html'
 	context_object_name = 'connections'
+
+	def get_context_data(self, **kwargs):
+		context = super(Connections, self).get_context_data()
+		query = self.request.GET.get("q")
+		if query:
+			users = User.objects.filter(username__icontains=query).exclude(pk=self.request.user.pk)
+			context['users'] = users		
+		return context
