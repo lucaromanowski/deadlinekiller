943a5ba2a534622bd8afbd81d110d3241cc02433
lucaromanowski <lucaromanowski@wp.pl>
2017-12-12 16:46:13 +0100

    Add friend view in progress
    post method works, form works, bootstrap button

--- deadlinekiller/friends/urls.py
+++ deadlinekiller/friends/urls.py
@@ -8,6 +8,7 @@ urlpatterns = [
 
 
     url(r'^connections/$', views.Connections.as_view(), name='connections'),
+    url(r'^make-connection/$', views.MakeConnectionView.as_view(), name='make_connection'),
     
 
 ]
--- deadlinekiller/friends/views.py
+++ deadlinekiller/friends/views.py
@@ -1,12 +1,13 @@
+from django.contrib.auth.mixins import LoginRequiredMixin
 from django.contrib.auth.models import User
 from django.db.models import Q
-from django.shortcuts import render
-from django.views.generic import ListView
+from django.shortcuts import render, redirect
+from django.views.generic import ListView, View
 
 from .models import Connection 
 
 
-class Connections(ListView):
+class Connections(LoginRequiredMixin, ListView):
 	model = Connection
 	template_name = 'friends/connections_list.html'
 	context_object_name = 'connections'
@@ -27,3 +28,14 @@ class Connections(ListView):
 
 			context['users'] = users		
 		return context
+
+
+class MakeConnectionView(LoginRequiredMixin, View):
+	
+	def post(self, request,  *args, **kwargs):
+		user = request.user
+		print('user: ', str(user))
+		post_data = request.POST['friend']
+		print('post data: ', str(post_data))
+		print('View, post method')
+		return redirect('friends:connections')
